0794a68d3e932bdddcdf634aaf6b4baae27341c8
yugaltikale <yugaltikale@gmail.com>
2026-06-21 12:33:12 +0530

    Update adpll_top.v

--- ADPLL_Integrated/adpll_top.v
+++ ADPLL_Integrated/adpll_top.v
@@ -5,7 +5,25 @@ module adpll_top(
     output signed [24:0]phase_error
 );
 
+wire [6:0] m1_reg;
+wire [6:0] F_mod;
+wire c2_prev;
+
+wire signed [24:0] phase_residual;
+wire [4:0] dtc_code;
+
 phase_detector pd_inst(.ref_clk(ref_clk), .fb_clk(fb_clk), .rst(rst), .phase_error(phase_error));
 
-
+dtc_model dtc_inst(
+    .clk(ref_clk),
+    .rst(rst),
+    .phase_error(phase_error),
+    .m1_reg(m1_reg),
+    .F_mod(F_mod),
+    .c2_prev(c2_prev),
+    .phase_residual(phase_residual),
+    .dtc_code(dtc_code)
+);
+// phase_residual will be connected to TDC input
+// m1_reg, F_mod and c2_prev will be connected from MASH module
 endmodule
