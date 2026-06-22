cfc6a6757155a56c130667942fb4af2453de2859
Pranav Suresh <pranavsuresh525@gmail.com>
2026-06-22 21:11:16 +0530

    Update cic_decimator.v

--- ADPLL_Integrated/cic_decimator.v
+++ ADPLL_Integrated/cic_decimator.v
@@ -1,12 +1,10 @@
 `timescale 1ns/1ps
 
-// CIC Decimator including a counter and is used to send the do_update signal and current_phi error signal
-
 module cic_decimator #(
     parameter DECIM = 4
 )(
     input  wire clk,
-    input  wire rst_n,
+    input  wire rst,        
     input  wire signed [24:0] phase_residual, 
     
     output reg  [15:0] counter,
@@ -14,19 +12,18 @@ module cic_decimator #(
     output reg  signed [31:0] current_phi_error
 );
 
-    // Accumulators and Differentiators
     reg signed [31:0] acc1, acc2;
     reg signed [31:0] acc2_z1, diff1_z1;
     reg [2:0] decim_cnt;
 
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
+    always @(posedge clk or posedge rst) begin 
+        if (rst) begin                         
             acc1 <= 0;
             acc2 <= 0;
             acc2_z1 <= 0;
             diff1_z1 <= 0;
             decim_cnt <= 0;
-            counter <= 0;       
+            counter <= 0;        
             do_update <= 0;
             current_phi_error <= 0;
         end else begin
