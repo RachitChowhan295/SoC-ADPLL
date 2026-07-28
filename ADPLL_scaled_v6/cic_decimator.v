a227e460d3d47c4fe22858e1e0eaed17fe4eda06
Aneesh-Vinayak <ee250002012@iiti.ac.in>
2026-07-28 23:09:10 +0530

    Update cic_decimator.v

--- ADPLL_scaled_v6/cic_decimator.v
+++ ADPLL_scaled_v6/cic_decimator.v
@@ -5,40 +5,40 @@ module cic_decimator #(
 )(
     input  wire clk,
     input  wire rst,        
-    input  wire signed [24:0] phase_residual, 
+    input  wire signed [15:0] phase_residual, 
     
     output reg  [15:0] counter,
     output reg  do_update,
-    output reg  signed [31:0] current_phi_error
+    output reg  signed [22:0] current_phi_error
 );
 
-    reg signed [31:0] acc1, acc2;
-    reg signed [31:0] acc2_z1, diff1_z1;
+    reg signed [22:0] acc1, acc2;
+    reg signed [22:0] acc2_z1, diff1_z1;
     reg [1:0] decim_cnt;
 
-    wire signed [31:0] diff1_now = acc2_next - acc2_z1;
-    wire signed [31:0] diff2_now = diff1_now - diff1_z1;
+    wire signed [22:0] diff1_now = acc2_next - acc2_z1;
+    wire signed [22:0] diff2_now = diff1_now - diff1_z1;
 
-    wire signed [31:0] acc1_next = acc1 + $signed({{7{phase_residual[24]}}, phase_residual});
-    wire signed [31:0] acc2_next = acc2 + acc1_next;
+    wire signed [22:0] acc1_next = acc1 + $signed({{7{phase_residual[24]}}, phase_residual});
+    wire signed [22:0] acc2_next = acc2 + acc1_next;
 
     parameter LOCK_COUNT = 12'd2000;
     reg [11:0]stable_counter;
-    wire [31:0] abs_error = current_phi_error[31] ? -current_phi_error : current_phi_error; 
-    parameter [31:0] THRESHOLD  = 32'd1000;
+    wire [22:0] abs_error = current_phi_error[22] ? -current_phi_error : current_phi_error; 
+    parameter [22:0] THRESHOLD  = 23'd1000;
     reg low_power_mode;
     reg toggle;
 
     always @(posedge clk or posedge rst) begin 
         if (rst) begin                         
-            acc1              <= 32'sd0;
-            acc2              <= 32'sd0;
-            acc2_z1           <= 32'sd0;
-            diff1_z1          <= 32'sd0;
+            acc1              <= 23'sd0;
+            acc2              <= 23'sd0;
+            acc2_z1           <= 23'sd0;
+            diff1_z1          <= 23'sd0;
             decim_cnt         <= 2'd0;
             counter           <= 16'd0;
             do_update         <= 1'b0;
-            current_phi_error <= 32'sd0;
+            current_phi_error <= 23'sd0;
             low_power_mode    <= 1'd0;
             stable_counter    <= 12'd0;
             toggle             <= 1'd0;
